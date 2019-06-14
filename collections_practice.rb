0a1f9f5893ed63e87e56848cd06201ee1cee29a3
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-14 16:56:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,13 @@
 require 'pry'
 
-def sort_array_asc(numbers)
+def sort_array_asc(array)
+  array.sort 
+end 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  
+end 
